904fcdbc42cfabab0ad0972632097cee9f369df6
Ammly <ammlyf@gmail.com>
2020-09-14 10:52:08 +0300

    containerize yolomy app

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,24 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://mongo:27017/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 let db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
+db.once('open', () => {
     console.log('Database connected successfully')
 })
 
 // Check for DB Errors
-db.on('error', (error)=>{
+db.on('error', (error) => {
     console.log(error);
 })
 
@@ -31,10 +34,10 @@ const app = express()
 // Body parser middleware
 app.use(express.json())
 
-// 
-app.use(upload.array()); 
+//
+app.use(upload.array());
 
-// Cors 
+// Cors
 app.use(cors());
 
 // Use Route
@@ -43,6 +46,6 @@ app.use('/api/products', productRoute)
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
